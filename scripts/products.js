33a19d5df9fe4202149b4d94173a89ed3b6f8069
TalhaShaikh150 <tssmart233@gmail.com>
2025-06-22 12:14:11 +0500

    Added Products Array To Dynamically Generate Products

---
+++ scripts/products.js
@@ -0,0 +1,35 @@
+export function products() {
+  const productData = [
+    {
+      id: 1,
+      img: "assets/singleproduct1.jpg",
+      name: "Premium Wireless HeadPhones",
+      rating: "assets/ratings/rating-15.png",
+      ratingCount: 124,
+      currency: "Rs ",
+      price: 299,
+      category: "Electronics",
+    },
+    {
+      id: 2,
+      img: "assets/singleproduct2.jpg",
+      name: "Premium Wireless HeadPhones",
+      rating: "assets/ratings/rating-15.png",
+      ratingCount: 124,
+      currency: "Rs ",
+      price: 299,
+      category: "Electronics",
+    },
+    {
+      id: 3,
+      img: "assets/singleproduct3.jpg",
+      name: "Premium Wireless HeadPhones",
+      rating: "assets/ratings/rating-15.png",
+      ratingCount: 124,
+      currency: "Rs ",
+      price: 299,
+      category: "Electronics",
+    },
+  ];
+  return productData;
+}
