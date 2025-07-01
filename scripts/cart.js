ac3cee00930a25dcf50ab76b8f9351b31fb23cdc
itssadiq <sadiqalishaikh009@gmail.com>
2025-07-01 16:51:08 +0500

    - Made cart page interactive.

--- scripts/cart.js
+++ scripts/cart.js
@@ -2,9 +2,22 @@
 import { productData } from "./products.js";
 
 export const cart = JSON.parse(localStorage.getItem("cart")) || [];
+let totalQuantity = 0;
+
+cart.forEach((cartItem) => {
+  totalQuantity += cartItem.quantity;
+});
+
+const headingEl = document.querySelector(".cart-heading");
+
+if (headingEl) {
+  headingEl.innerHTML = `Shopping Cart(${totalQuantity} items)`;
+}
+
+renderCartSummary();
+renderPaymentSummary();
 
 //Add To Cart
-
 
 export function addToCart(cartQuantity, index) {
   let product;
@@ -17,14 +30,14 @@ export function addToCart(cartQuantity, index) {
     return; // Page not supported
   }
 
-let existingItem = undefined;
+  let existingItem = undefined;
 
   cart.forEach((item) => {
     if (item.id === product.id) {
       existingItem = item;
     }
   });
-  
+
   if (existingItem) {
     existingItem.quantity += 1;
   } else {
@@ -35,8 +48,90 @@ let existingItem = undefined;
   saveToStorage(cart);
 }
 
-
 //Save To Local Storage
 export function saveToStorage(cart) {
   localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+function renderCartSummary() {
+  let cartItemsHTML = "";
+
+  cart.forEach((cartItem) => {
+    const html = `
+      <div class="cart-item">
+          <div class="image-name">
+            <img
+              src="${cartItem.images[0].img}"
+              alt=""
+              class="product-image"
+            />
+            <div class="name-price">
+              <h4>${cartItem.name}</h4>
+              <p class="price">Rs ${cartItem.price}</p>
+              <p class="quantity">Quantity: ${cartItem.quantity}</p>
+            </div>
+          </div>
+          <div class="cart-item-handles">
+            <span class="update-quantity">Update</span>
+            <input type="text" class="quantity-input" />
+            <span class="save-quantity">Save</span>
+
+            <i class="fa-solid fa-trash"></i>
+            <p class="calculated-price">Rs ${
+              cartItem.price * cartItem.quantity
+            }</p>
+          </div>
+        </div>
+    `;
+
+    cartItemsHTML += html;
+  });
+
+  const cartItemsEl = document.querySelector(".main-cart-items");
+
+  if (cartItemsEl) {
+    cartItemsEl.innerHTML = cartItemsHTML;
+  }
+}
+
+function renderPaymentSummary() {
+  let paymentSummaryHTML = "";
+
+  let price = 0;
+
+  cart.forEach((cartItem) => {
+    price += cartItem.price * cartItem.quantity;
+  });
+
+  const html = `
+    <h3>Order Summary</h3>
+        <div class="payment-summary">
+          <div class="payment-summary-row">
+            <div class="payment-summary-title">Items(${totalQuantity}):</div>
+            <div class="payment-summary-money">Rs ${price}</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div class="payment-summary-title">Shipping & handling:</div>
+            <div class="payment-summary-money">Rs 100</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div class="payment-summary-title">Estimated Tax:</div>
+            <div class="payment-summary-money">Rs 50</div>
+          </div>
+        </div>
+        <div class="payment-summary-row order-total">
+          <div class="payment-summary-title"><h3>Total</h3></div>
+          <div class="payment-summary-money total-money"><h3>Rs ${
+            price + 150
+          }</h3></div>
+        </div>
+  `;
+
+  const cartSummaryEl = document.querySelector(".cart-summary");
+
+  if (cartSummaryEl) {
+    cartSummaryEl.innerHTML = html;
+  }
 }
